61d7bdb0171ef73b323ade495a84db3a6dc35766
aberdeens <54465985+aberdeens@users.noreply.github.com>
2022-09-27 09:04:43 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ const App = () => {
 
   // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
   // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
-
   return (
     <div className="App">
       <Grid container spacing={2}>
